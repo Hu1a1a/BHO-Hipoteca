163d850a5d83d3ddb8620090fdfeb7d0e8775444
Hulala <yang.ye.1@hotmail.com>
2025-07-06 22:55:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo desde Express!');
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
