2ae2c76ac8fc7feee183aba1f4b5d39af437c695
Hulala <yang.ye.1@hotmail.com>
2025-07-07 20:24:24 +0200

    Update Hipoteca index.js

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const cors = require('cors');           // opcional, pero útil si llamas desde otro dominio
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());                           // quítalo si no lo necesitas
+app.use(express.json());                   // para peticiones con JSON
+app.use(express.urlencoded({ extended: true })); // para application/x-www-form-urlencoded
 
-// Middleware para parsear JSON
-app.use(express.json());
 
-// Ruta de prueba
 app.get('/', (req, res) => {
-    res.send('¡Hola, mundo desde Express!');
+    res.send('HBO Hipoteca Server');
 });
 
-// Iniciar el servidor
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.get('/form/send', (req, res) => {
+    res.status(200).json({
+        status: 'ok',
+        msg: 'Se ha recibido el body correctamente',
+        data: req.body,
+    });
 });
+
+app.listen(PORT);
