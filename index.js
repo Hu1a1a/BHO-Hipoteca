0e1a065bbb4cbe884c1151e048449bb60b0f43ab
Hulala <yang.ye.1@hotmail.com>
2025-07-08 22:54:42 +0200

    Update form

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 const express = require('express');
-const cors = require('cors');           // opcional, pero útil si llamas desde otro dominio
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(cors());                           // quítalo si no lo necesitas
-app.use(express.json());                   // para peticiones con JSON
-app.use(express.urlencoded({ extended: true })); // para application/x-www-form-urlencoded
+require('./server')
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
     res.send('HBO Hipoteca Server');
 });
 
-app.get('/form/send', (req, res) => {
-    res.status(200).json({
-        status: 'ok',
-        msg: 'Se ha recibido el body correctamente',
-        data: req.body,
-    });
-});
 
-app.listen(PORT);
+app.listen(3000);
